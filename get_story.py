e8e952d8c947afe67fda09aa8d6b4b5cb6d3cc43
RedDirtSixKiller <adam@ajpconline.com>
2023-05-12 12:34:43 -0700

    get_story class started

--- get_story.py
+++ get_story.py
@@ -1,7 +1,7 @@
 import openai
+import config
 
-api_key = 'getenv'
-
+openai.api_key = config.openai_key
 
 #the main workhorse
 def get_completion(prompt, model="gpt-3.5-turbo"):
@@ -12,3 +12,22 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
         temperature=0, # this is the degree of randomness of the model's output
     )
     return response.choices[0].message["content"]
+
+#chars should come from the character class
+#need to write a class for the action - probably with a sub action for attacks?
+char1 = 'Daisy the drow elf'
+char2 = 'Hokar the half-orc'
+act = 'uses a bow to shoot'
+
+prompt = f"""
+Your task is to act as a dungeon master describing a combat action in dungeons and dragons.\
+ Write a narrative where the characters' information is delimited by <char></char>\
+ and the action is delimited by <act></act>
+ 
+ Action: <char>{char1}</char> <act>{act}</act> <char>{char2}</char>
+"""
+
+
+#uncommenting here will cost money, not much but it isn't free
+#response = get_completion(prompt)
+#print(response)
