92051a41fa65981771ce0d981f0a1e7f6283fa56
RedDirtSixKiller <adam@ajpconline.com>
2023-05-12 10:42:01 -0700

    started chat class and added config in ignore

---
+++ get_story.py
@@ -0,0 +1,14 @@
+import openai
+
+api_key = 'getenv'
+
+
+#the main workhorse
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
